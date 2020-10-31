25c03c923cd73aa13121634fbb16076373ffce38
cyberknight01 <cyberknight.hs@icloud.com>
2020-10-31 17:50:22 +0800

    update

--- Chapter1/chapter1-printf.py
+++
@@ -1,7 +0,0 @@
-from ctypes import *
-msvcrt = cdll.msvcrt
-#message_string = b"Hello world!\n"
-#msvcrt.printf(b"Testing:%s",message_string)
-message_string = "Hello world!\n"
-result = message_string.encode("utf-8")
-msvcrt.printf(b"Testing:%s",result)
--- Chapter3/my_test.py
+++ Chapter3/my_test.py
@@ -1,5 +1,7 @@
 import my_debugger
 
 debugger = my_debugger.debugger()
-
-debugger.load("C:\\WINDOWS\\system32\\calc.exe")
+pid = input("请输入待调试进程的进程号:")
+# 由于 input 函数的性质，将输入的字符转换为数字型
+debugger.attach(int(pid))
+debugger.detach()
